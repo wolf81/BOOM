632325d72346365874c1940b8fd3b7bfbd64afe6
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2021-08-22 06:08:52 +0700

    Add BOOM assets

--- main.lua
+++ main.lua
@@ -8,6 +8,11 @@ function love.load(args)
     local contents, size = love.filesystem.read('version.txt')
     local version = contents:gsub('_', '.')
 
+    local modes = love.window.getFullscreenModes(1)
+    for _, mode in ipairs(modes) do
+    	print(mode.width, mode.height)
+    end
+
 	love.window.setTitle('BOOM (' .. version .. ')')
 end
 
