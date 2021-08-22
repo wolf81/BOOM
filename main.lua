fbcf896595afa491da9b20d16c8ed34a5d047f8a
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2021-08-22 12:45:55 +0700

    Code clean-up

--- main.lua
+++ main.lua
@@ -13,17 +13,6 @@ borders = {
     ['0'] = love.graphics.newImage('gfx/Border 01.png')    
 }
 
-borderQuads = {
-    ['L'] = love.graphics.newQuad(0, 0, 32, 32, 256, 32),
-    ['R'] = love.graphics.newQuad(32, 0, 32, 32, 256, 32),
-    ['U'] = love.graphics.newQuad(64, 0, 32, 32, 256, 32),
-    ['D'] = love.graphics.newQuad(96, 0, 32, 32, 256, 32),
-    ['DL'] = love.graphics.newQuad(128, 0, 32, 32, 256, 32),
-    ['DR'] = love.graphics.newQuad(160, 0, 32, 32, 256, 32),
-    ['UR'] = love.graphics.newQuad(196, 0, 32, 32, 256, 32),
-    ['UL'] = love.graphics.newQuad(228, 0, 32, 32, 256, 32),
-}
-
 function love.load(args)
 	print('load')
 
--- src/utilities/background.lua
+++ src/utilities/background.lua
@@ -6,6 +6,17 @@ TileSize = {
 	['HEIGHT'] = 32,
 }
 
+local BorderQuads = {
+    ['L'] = love.graphics.newQuad(0, 0, 32, 32, 256, 32),
+    ['R'] = love.graphics.newQuad(32, 0, 32, 32, 256, 32),
+    ['U'] = love.graphics.newQuad(64, 0, 32, 32, 256, 32),
+    ['D'] = love.graphics.newQuad(96, 0, 32, 32, 256, 32),
+    ['DL'] = love.graphics.newQuad(128, 0, 32, 32, 256, 32),
+    ['DR'] = love.graphics.newQuad(160, 0, 32, 32, 256, 32),
+    ['UR'] = love.graphics.newQuad(196, 0, 32, 32, 256, 32),
+    ['UL'] = love.graphics.newQuad(228, 0, 32, 32, 256, 32),
+}
+
 local function newBackgroundCanvas(background, border)
 	local lw, lh = (Map.WIDTH + 2) * TileSize.WIDTH, (Map.HEIGHT + 2) * TileSize.HEIGHT
 	local canvas = love.graphics.newCanvas(lw, lh)
@@ -23,21 +34,21 @@ local function newBackgroundCanvas(background, border)
 		end
 
 		for x = 1, Map.WIDTH do
-			love.graphics.draw(border, borderQuads['U'], x * TileSize.WIDTH, 0)
+			love.graphics.draw(border, BorderQuads['U'], x * TileSize.WIDTH, 0)
 			local y = (Map.HEIGHT + 1) * TileSize.HEIGHT
-			love.graphics.draw(border, borderQuads['D'], x * TileSize.WIDTH, y)
+			love.graphics.draw(border, BorderQuads['D'], x * TileSize.WIDTH, y)
 		end	
 
 		for y = 1, Map.HEIGHT do
-			love.graphics.draw(border, borderQuads['L'], 0, y * TileSize.HEIGHT)
+			love.graphics.draw(border, BorderQuads['L'], 0, y * TileSize.HEIGHT)
 			local x = (Map.WIDTH + 1) * TileSize.WIDTH
-			love.graphics.draw(border, borderQuads['R'], x, y * TileSize.HEIGHT)
+			love.graphics.draw(border, BorderQuads['R'], x, y * TileSize.HEIGHT)
 		end
 
-		love.graphics.draw(border, borderQuads['UL'], 0, 0)
-		love.graphics.draw(border, borderQuads['UR'], (Map.WIDTH + 1) * TileSize.WIDTH, 0)
-		love.graphics.draw(border, borderQuads['DL'], 0, (Map.HEIGHT + 1) * TileSize.HEIGHT)
-		love.graphics.draw(border, borderQuads['DR'], (Map.WIDTH + 1) * TileSize.WIDTH, (Map.HEIGHT + 1) * TileSize.HEIGHT)
+		love.graphics.draw(border, BorderQuads['UL'], 0, 0)
+		love.graphics.draw(border, BorderQuads['UR'], (Map.WIDTH + 1) * TileSize.WIDTH, 0)
+		love.graphics.draw(border, BorderQuads['DL'], 0, (Map.HEIGHT + 1) * TileSize.HEIGHT)
+		love.graphics.draw(border, BorderQuads['DR'], (Map.WIDTH + 1) * TileSize.WIDTH, (Map.HEIGHT + 1) * TileSize.HEIGHT)
 	end
 
 	love.graphics.setCanvas()
