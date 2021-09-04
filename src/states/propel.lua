df00e06fdffc668e8c681bd18697232517c4e047
Wolfgang Schreurs <wolfgang@wolftrail.net>
2021-09-04 17:05:20 +0700

    WIP: show destroy animation for projectiles

--- src/states/propel.lua
+++ src/states/propel.lua
@@ -8,9 +8,18 @@ function Propel:update(dt)
 	Propel.super.update(self, dt)
 
 	local entity = self.entity
+
+	local gridPos = entity:gridPosition()
 	local dxy = entity:velocity():permul(TileSize) * dt * entity:speed()
 	local pos = entity:position() + dxy
 	entity:setPosition(pos)
+
+	local toGridPos = entity:gridPosition()
+	if entity:level():isBlocked(toGridPos) then
+		toPos = toPosition(toGridPos) + vector(16, 8)
+		entity:setPosition(toPos)
+		entity:destroy()
+	end
 end
 
 function Propel:draw()
