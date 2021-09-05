be6f0e85005f07fd3ea149ad8c23ee1c2df99012
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2021-09-05 12:41:34 +0700

    Despawn bonuses after 10 seconds, show flicker animation after 7 seconds

---
+++ src/states/despawn.lua
@@ -0,0 +1,17 @@
+Despawn = State:extend()
+
+function Despawn:new()
+	-- body
+end
+
+function Despawn:enter(params)
+	-- body
+end
+
+function Despawn:update(dt)
+	-- body
+end
+
+function Despawn:draw()
+	-- body
+end
