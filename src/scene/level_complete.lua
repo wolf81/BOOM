20d23ba5f02dfd697777cde38c56451a98766f52
Wolfgang Schreurs <wolfgang@wolftrail.net>
2023-02-15 07:51:54 +0700

    Improve extra animation

--- src/scene/level_complete.lua
+++ src/scene/level_complete.lua
@@ -22,7 +22,20 @@ end
 
 local function proceedNextLevel(self)
 	local level, background = LevelLoader.load(self.level.index + 1)
-	print('=>', self.level.index)
+
+	-- copy player state over from player in last level to player in next level
+	for _, old_player in ipairs(self.level.players) do
+		for _, new_player in ipairs(level.players) do
+			if old_player.name == new_player.name then
+				new_player.bonus_flags = old_player.bonus_flags
+				new_player.extra_flags = old_player.extra_flags
+				new_player.score = old_player.score
+				new_player.lives = old_player.lives
+				new_player.hitpoints = old_player.hitpoints
+			end
+		end
+	end
+
 	if level then
 		Transition.crossfade(self, Game, level, background)
 	else
@@ -38,11 +51,16 @@ function LevelComplete:init()
 end
 
 function LevelComplete:enter(previous, level)
-	print('ENTER LEVEL COMPLETE')
 	self.level = level
 	self.hud = Hud(self.level)
 
-	self.text = 'TIME BONUS!'
+	self.message1 = 'TIME BONUS!'
+	self.message2 = math_floor(self.level.time) * 10
+
+	-- remove temporary bonuses
+	for _, player in ipairs(self.level.players) do
+		player.bonus_flags = ClearMask(player.bonus_flags, bit.bor(BonusFlags.SHIELD, BonusFlags.BOOTS))
+	end
 
 	Timer.after(3.0, function() proceedNextLevel(self) end)
 end
@@ -56,12 +74,15 @@ function LevelComplete:draw()
 	love.graphics.translate(HUD_W, 0)
 	love.graphics.draw(self.background)
 
-
 	love.graphics.setFont(self.font)
 	love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
-	local w = self.font:getWidth(self.text)
-	local h = self.font:getHeight()
-	love.graphics.print('TIME BONUS!', math_floor((WINDOW_W - HUD_W - w) / 2), math_floor((WINDOW_H - h) / 2))
+	local h = self.font:getHeight() + 10
+
+	local w = self.font:getWidth(self.message1)
+	love.graphics.print(self.message1, math_floor((WINDOW_W - HUD_W - w) / 2), math_floor(WINDOW_H / 2 - h / 2))
+
+	w = self.font:getWidth(self.message2)
+	love.graphics.print(self.message2, math_floor((WINDOW_W - HUD_W - w) / 2), math_floor(WINDOW_H / 2 + h / 2))
 
 	love.graphics.pop()
 
